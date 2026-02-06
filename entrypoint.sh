a009d5d40e9236a52c04cee5f400d09e537a2e47
Kylie Herbstzuber <kmherbstzuber@wpi.edu>
2026-02-06 14:55:08 -0500

    robot moves, dockerfile updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 # Source ROS 2
 source /opt/ros/jazzy/setup.bash
 
+# colcon build
+colcon build --symlink-install
+
 # Only source workspace if it exists (bind mount will make it available)
 if [ -f /ros2_ws/install/setup.bash ]; then
     source /ros2_ws/install/setup.bash
